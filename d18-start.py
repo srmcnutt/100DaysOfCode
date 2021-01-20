649086ecc4285bec2b514955c18a41a8f4f1bdda
Steve McNutt <mcnutt@lcti.net>
2021-01-20 07:18:50 -0500

    day 18 start

---
+++ d18-start.py
@@ -0,0 +1,9 @@
+import turtle as t
+import random 
+
+tim = t.Turtle()
+
+########### Challenge 4 - Random Walk ########
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+

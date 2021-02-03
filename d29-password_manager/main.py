de5eeafbe81e66f601f4bab5addb0b8bf0aaa9a5
Steven McNutt <stmcnutt@cisco.com>
2021-02-03 07:37:54 -0500

    day 30

--- d29-password_manager/main.py
+++ d29-password_manager/main.py
@@ -2,12 +2,36 @@ from tkinter import *
 from tkinter import messagebox
 import pyperclip
 import random
+import json
+
+#----------------------------- Search ---------------------------------------#
+def search():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="error", message="datafile not found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website,
+                                message=f"website: {website}\n password: {password}")
+        else:
+            messagebox.showinfo(title="record not found", message="website not found")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+                }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="oops", message="Make sure you haven't left any fields empty")
@@ -21,10 +45,20 @@ def save():
         )
 
         if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+            try:
+                with open("data.json", "r") as file:
+                    data = json.load(file)
+            except FileNotFoundError:
+                with open("data.json", "w") as file:
+                    data = json.dump(new_data, file, indent=4)
+
+            else:
+                with open("data.json", "w") as file:
+                    data.update(new_data)
+                    json.dump(data, file, indent=4)
+
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -74,19 +108,21 @@ password_label = Label(text="password")
 password_label.grid(row=3, column=0)
 
 # entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
-email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
+email_entry = Entry(width=21)
+email_entry.grid(row=2, column=1, columnspan=1)
 email_entry.insert(0, "bubba@bubbas123.com")
 password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
+password_entry.grid(row=3, column=1, columnspan=1)
 
 # buttons
+search_button = Button(text="Search", width=13, command=search)
+search_button.grid(row=1, column=2)
 generate_password_buttom = Button(text="Generate Password", command=generate_password)
 generate_password_buttom.grid(row=3, column=2)
-add_button = Button(text="Add", width=36, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
+add_button = Button(text="Add", width=21, command=save)
+add_button.grid(row=4, column=1, columnspan=1)
 
 window.mainloop()
